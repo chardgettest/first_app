494b84222529dcf09ea2f42ecb4a23fdc2e2554d
Chardget Test <chardgettest@gmail.com>
2013-03-20 22:14:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a03ee42ac4394c94cec99739fa041fbd377f7c0785651a3495c460a6ed273f2c01c4eb787140bda89dffd66f8fbc247173224f58061d0d19b7096100f62ce5e4'
